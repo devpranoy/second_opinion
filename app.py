789a21755b0e4d95c30056fb72e4886642a283d1
Pranoy Dev <devpranoy@gmail.com>
2019-02-20 13:53:13 +0530

    Created: Files for second

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # importing packages
 from flask import Flask ,render_template, redirect, url_for, session, request, logging
-#import requests
-#import json
+import requests
+import json
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,8 +9,12 @@ import torch
 from torch import nn, optim
 from torchvision import transforms, datasets, models
 from torch.utils.data.sampler import SubsetRandomSampler
+from werkzeug.utils import secure_filename
+import os
 
-from werkzeug import secure_filename
+# UPLOAD_FOLDER = '/Users/pranoy/Desktop/second_opinion/uploads/'
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 model = models.resnet50(pretrained=True)
 
 for param in model.parameters():
@@ -22,7 +26,7 @@ fc_parameters = model.fc.parameters()
 
 for param in fc_parameters:
     param.requires_grad = True
-model.load_state_dict(torch.load('/home/ubuntu/checkpoints.pt',map_location='cpu'))
+model.load_state_dict(torch.load('checkpoints.pt',map_location='cpu'))
 criterion = nn.CrossEntropyLoss()
 def load_input_image(img_path):    
     image = Image.open(img_path)
@@ -41,6 +45,7 @@ def predict_malaria(model, class_names, img_path):
     model = model.cpu()
     model.eval()
     idx = torch.argmax(model(img))
+    
     return class_names[idx]
 
 
@@ -54,13 +59,36 @@ class_names=['Parasitized','Uninfected']
 
 
 app = Flask(__name__) #app initialisation
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/android', methods=['GET','POST'])
+def androidapi():
+    if request.method=='POST':
+        x= request.get_json()
+        img_string= x['Image']
+        import base64
+        imagedata = base64.b64decode(img_string)
+        file = "upload.png"
+        with open (file,'wb') as f:
+            f.write(imagedata)
+        if predict_malaria(model, class_names, file) == 'Parasitized':
+            flag =1 #parasite detected
+        else:
+            flag =0
+        if flag ==1:
+            print("Parasite")
+            return "Parasite"
+        else:
+            print("No Parasite")
+            return "No Parasite"
+    return "Error"
+
 
 @app.route('/get_label', methods=['GET','POST']) #landing page intent
 def label():
     if request.method=='POST':
-        title = request.form['title']
-        remark = request.form['remark']
-        app.config['UPLOAD_FOLDER']="/home/ubuntu/second_opinion/static/uploads/"
+        app.config['UPLOAD_FOLDER']="/Users/pranoy/Desktop/second_opinion/uploads/"
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
             os.makedirs(app.config['UPLOAD_FOLDER'])
         try:
@@ -68,16 +96,40 @@ def label():
             f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
             path = app.config['UPLOAD_FOLDER']+'/'+f.filename
             img_path = np.array(glob(path))
-            img = Image.open(img_path)
+            # img = Image.open(img_path)
+            print("REached")
             if predict_malaria(model, class_names, img_path) == 'Parasitized':
                 return "Infected"
             else:
                 return "Un-Infected"
         except:
             print("error")
+        
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET','POST'])
 def home():
+    if request.method=='POST':
+        app.config['UPLOAD_FOLDER']="/Users/pranoy/Desktop/second_opinion/uploads/"
+        if not os.path.exists(app.config['UPLOAD_FOLDER']):
+            os.makedirs(app.config['UPLOAD_FOLDER'])
+        # try:
+        f = request.files['file']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+        path = app.config['UPLOAD_FOLDER']+f.filename
+        img_path = path
+
+        if predict_malaria(model, class_names, img_path) == 'Parasitized':
+            print("Infected")
+            return render_template("resultinfected.html")
+        else:
+            print("unInfected")
+            return render_template("resultuninfected.html")
+        # except Exception as e:
+        #     print(e)
     return render_template("index.html") #display the html template
 
 if __name__=='__main__':
